2f39b6eaa085656a6c98029a74a1200cfcd74e84
Artem Troy <coupdegr@gmail.com>
2022-08-17 13:06:48 +0300

    Loading

--- todo/src/components/login/AuthForm.tsx
+++ todo/src/components/login/AuthForm.tsx
@@ -1,38 +1,45 @@
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
 import KeyIcon from "@mui/icons-material/Key";
 import LockIcon from "@mui/icons-material/Lock";
 import PersonIcon from "@mui/icons-material/Person";
 import {
   Box,
   Button,
-  FormControl,
   FormHelperText,
   Input,
   InputLabel,
   Link,
   Paper,
-  Stack,
-  TextField,
+  Stack
 } from "@mui/material";
 import { Formik } from "formik";
-import { useRef } from "react";
-import styled from "styled-components";
 import { signInSchema } from "../../validation/signInFormValidation";
 import {
   StyledFieldBox,
-  StyledTextField,
+  StyledFormControl,
   StyledGitHubIcon,
-  StyledInstagramIcon,
+  StyledInstagramIcon
 } from "./styledAuthForm";
 
 interface AuthFormValues {
   login: string;
   password: string;
 }
+interface NumberButtonProps {
+  val: string;
+}
 
+export function IconChange({ val }: NumberButtonProps) {
+  let icon: JSX.Element;
+  if (val.includes("@")) {
+    icon = <EmailIcon fontSize="large" sx={{ mx: 0.3 }} />;
+  } else {
+    icon = <PersonIcon fontSize="large" sx={{ mx: 0.3 }} />;
+  }
+
+  return <>{icon}</>;
+}
 const AuthForm = () => {
-  const textHelperRef = useRef();
   const initialValues: AuthFormValues = {
     login: "",
     password: "",
@@ -66,73 +73,56 @@ const AuthForm = () => {
             sx={{ mb: 2 }}
             spacing={2}
           >
-            <StyledFieldBox isError={!formik.errors.login}>
-              <PersonIcon fontSize="large" sx={{ mr: 0.5 }} />
-
-              <StyledTextField
-                inputRef={textHelperRef}
+            <StyledFieldBox>
+              <IconChange val={formik.values.login} />
+              <StyledFormControl
                 error={formik.touched.login && !!formik.errors.login}
-                sx={{ width: "100%", mr: 1 }}
-                id="login"
-                label="Логин"
                 variant="standard"
-                {...formik.getFieldProps("login")}
-                name="login"
-                helperText={
-                  formik.values.login &&
-                  !!formik.errors.login &&
-                  formik.errors.login
-                }
+                sx={{ width: "100%", mr: 1 }}
                 $isError={!!formik.touched.login && !!formik.errors.login}
-              />
+              >
+                <InputLabel htmlFor="login ">Логин или email</InputLabel>
+                <Input
+                  {...formik.getFieldProps("login")}
+                  type="login"
+                  id="login"
+                />
+
+                <FormHelperText>
+                  {!!formik.errors.login && formik.values.login
+                    ? formik.errors.login
+                    : " "}
+                </FormHelperText>
+              </StyledFormControl>
             </StyledFieldBox>
-            <StyledFieldBox isError={!formik.errors.password}>
-              <FormControl
+
+            <StyledFieldBox>
+              <KeyIcon fontSize="large" sx={{ mx: 0.3 }} />
+              <StyledFormControl
                 error={formik.touched.password && !!formik.errors.password}
                 variant="standard"
                 sx={{ width: "100%", mr: 1 }}
+                $isError={!!formik.touched.password && !!formik.errors.password}
               >
-                <KeyIcon fontSize="large" sx={{ mx: 0.3 }} />
                 <InputLabel htmlFor="password ">Пароль</InputLabel>
                 <Input
                   {...formik.getFieldProps("password")}
                   type="password"
                   id="password"
-                ></Input>
-                <FormHelperText></FormHelperText>
-              </FormControl>
-            </StyledFieldBox>
-            <StyledFieldBox isError={!formik.errors.password}>
-              <KeyIcon fontSize="large" sx={{ mx: 0.3 }} />
-              {/*Добавить иконку просмотра пароля*/}
-              <StyledTextField
-                aria-label="password"
-                sx={{ width: "100%", mr: 1 }}
-                error={formik.touched.password && !!formik.errors.password}
-                helperText={
-                  formik.values.password &&
-                  !!formik.errors.password &&
-                  formik.errors.password
-                }
-                type="password"
-                id="password"
-                label="Пароль"
-                variant="standard"
-                $isError={!!formik.touched.password && !!formik.errors.password}
-                {...formik.getFieldProps("password")}
-              />
-              <FormHelperText>asddsa</FormHelperText>
+                />
+
+                <FormHelperText>
+                  {!!formik.errors.password && formik.values.password
+                    ? formik.errors.password
+                    : " "}
+                </FormHelperText>
+              </StyledFormControl>
             </StyledFieldBox>
 
             <Button disabled={!formik.isValid} type="submit" size="large">
               Войти
             </Button>
-            <Button
-              onClick={() => console.log(textHelperRef.current)}
-              size="large"
-            >
-              Регистрация
-            </Button>
+            <Button size="large">Регистрация</Button>
           </Stack>
         )}
       </Formik>
