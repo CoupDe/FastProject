709b557040b163d179f88d80ee2d239aabf1e92a
Artem Troy <coupdegr@gmail.com>
2022-12-07 14:23:42 +0300

    Доработка API

--- API/todo/todoApi/views.py
+++ API/todo/todoApi/views.py
@@ -1,17 +1,37 @@
+from urllib import response
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import Task
-from .serializers import TaskSerializers
+from rest_framework.response import Response
+
+from authentication.models import TodoUser
+from .models import Task, TaskComment
+from .serializers import CommentSerializers, TaskSerializers
+from rest_framework import status
 
 
 class TaskList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    # (IsAuthenticated,)
+    permission_classes = (AllowAny,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
 
 
 class TaskRetriveUpdate(generics.RetrieveUpdateAPIView):
     # Так как это ленивый запрос(обрабатывается только по запросоу) вернется единичный запрос
+    permission_classes = (AllowAny,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
+
+
+class TaskCommentList(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+
+    serializer_class = CommentSerializers
+
+    def post(self, request, pk):
+   
+        user = TodoUser.objects.get(pk=request.data['comment_creator'])
+        
+        print('user', user)
+        return Response({'title': 'Jennifer Shrader Lawrence'})
